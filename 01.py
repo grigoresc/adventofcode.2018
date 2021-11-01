c5cfea870dcfb8246bdc64c24d1ce7dcab062f91
csg <cgrigore@gmail.com>
2021-11-01 12:27:45 +0200

    1st problem

---
+++ 01.py
@@ -0,0 +1,21 @@
+import itertools
+# l = [+3, +3, +4, -2, -4]
+
+l = [int(line.strip()) for line in open("01.txt", 'r')]
+print(l)
+
+sln1 = sum(l) 
+print(sln1)
+
+c=itertools.cycle(l)
+
+a=0
+s = []
+for x in c:
+   a += x
+   if a in s:
+       print(a)
+       break
+   else:
+        s.append(a)
+        print(a)

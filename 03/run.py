a22e6fff26e452708e37c90125ce6477e515c367
Ciprian Grigore <grigoresc@outlook.com>
2021-11-02 14:04:54 +0200

    3rd problem

---
+++ 03/run.py
@@ -0,0 +1,43 @@
+import re
+import itertools
+inp = ["#1 @ 1,3: 4x4",
+"#2 @ 3,1: 4x4",
+"#3 @ 5,5: 2x2"]
+inp = [line.strip() for line in open("input.txt", 'r')]
+
+
+# ugly init..
+leng = 2000 
+a=[]
+la=[]
+for i in range(leng):
+    a.append(list(itertools.repeat(0,leng)))
+    la.append(list(itertools.repeat(0,leng)))
+
+untouched=set()
+def fill(no,l,t,w,h):
+    untouched.add(no)
+    for i in range(l,l+w):
+        for j in range(t,t+h):
+            a[j][i]+=1
+            if la[j][i]>0:
+                if la[j][i] in untouched:
+                    untouched.remove(la[j][i]) 
+                if no in untouched:
+                    untouched.remove(no)
+            la[j][i]=no
+
+for line in inp:
+    (no,c,pos,siz) = re.split(" ",line)
+    (l,t)=re.split(",",pos[:-1])
+    (w,h)=re.split("x",siz) 
+    fill(int(no[1:]),int(l),int(t),int(w),int(h))
+
+cnt=0
+for line in a:
+    cnt+=len(list(filter(lambda x:x>1,line)))
+
+print(cnt)
+#116489
+print(untouched)
+#1260

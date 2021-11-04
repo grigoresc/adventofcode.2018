fc7dba3537b1b4548dec9bff76159d391ddfe467
Ciprian Grigore <grigoresc@outlook.com>
2021-11-04 21:09:01 +0200

    tweaks

--- 03/run.py
+++ 03/run.py
@@ -1,40 +1,38 @@
 import re
-import itertools
 inp = ["#1 @ 1,3: 4x4",
-"#2 @ 3,1: 4x4",
-"#3 @ 5,5: 2x2"]
+       "#2 @ 3,1: 4x4",
+       "#3 @ 5,5: 2x2"]
 inp = [line.strip() for line in open("input.txt", 'r')]
 
+leng = 2000
+a = [[0 for i in range(leng)] for j in range(leng)]
+la = [[0 for i in range(leng)] for j in range(leng)]
 
-# ugly init..
-leng = 2000 
-a=[]
-la=[]
-for i in range(leng):
-    a.append(list(itertools.repeat(0,leng)))
-    la.append(list(itertools.repeat(0,leng)))
+untouched = set()
 
-untouched=set()
-def fill(no,l,t,w,h):
+
+def fill(no, l, t, w, h):
     untouched.add(no)
-    for i in range(l,l+w):
-        for j in range(t,t+h):
-            a[j][i]+=1
-            if la[j][i]>0:
+    for i in range(l, l+w):
+        for j in range(t, t+h):
+            a[j][i] += 1
+            if la[j][i] > 0:
                 if la[j][i] in untouched:
-                    untouched.remove(la[j][i]) 
+                    untouched.remove(la[j][i])
                 if no in untouched:
                     untouched.remove(no)
-            la[j][i]=no
-for line in inp:
-    (no,l,t,w,h) = map(int,re.findall(r'\d+',line))
-    fill(no,l,t,w,h)
+            la[j][i] = no
 
-cnt=0
+
+for line in inp:
+    (no, l, t, w, h) = map(int, re.findall(r'\d+', line))
+    fill(no, l, t, w, h)
+
+cnt = 0
 for line in a:
-    cnt+=len(list(filter(lambda x:x>1,line)))
+    cnt += len(list(filter(lambda x: x > 1, line)))
 
 print(cnt)
-#116489
+# 116489
 print(untouched)
-#1260
+# 1260

eed38a67acf5526922dfc60fdba17290ae961d73
Ciprian Grigore <grigoresc@outlook.com>
2021-11-14 13:47:14 +0200

    day 9 1st problem solved

---
+++ 09/run.py
@@ -0,0 +1,44 @@
+def sln1(playersno, marblesno):
+    circle = [0, 1]
+    current = 1
+    currentPlayer = 2
+    playerscores = [0 for i in range(0, playersno)]
+    for m in range(2, marblesno+1):
+        # if m % 50000 == 0:
+        #     print(">", m, len(circle), max(
+        #         playerscores), current, currentPlayer)
+        if m % 23 == 0:
+            playerscores[currentPlayer] += m
+            delepos = current - 7
+
+            delepos = delepos + len(circle) if delepos < 0 else delepos
+            retdele = circle.pop(delepos)
+            playerscores[currentPlayer] += retdele
+            nextpos = delepos
+            if nextpos == len(circle) - 1:
+                # strange that this case is never reached..
+                nextpos = 0
+
+        else:
+            nextpos = (current+2) % len(circle)
+            circle.insert(nextpos, m)
+
+        current = nextpos
+        currentPlayer += 1
+        currentPlayer = currentPlayer % playersno
+    # print(playerscores)
+    print(max(playerscores))
+    # print(circle)
+
+
+# samples
+sln1(9, 25)
+sln1(10, 1618)
+sln1(13, 7999)
+sln1(17, 1104)
+sln1(21, 6111)
+sln1(30, 5807)
+# input
+sln1(459, 72103)
+# 388131
+sln1(459, 72103*100)
